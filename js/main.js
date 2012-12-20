0ca001cf073d7c566d8c5e48f27962af1b189467
Chris Mytton <self@hecticjeff.net>
2012-12-20 22:24:15 +0000

    Don't adjust viewport on iPad

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function() {
   new FastClick(document.body);
 
   // Hide the address bar on iOS - http://davidwalsh.name/hide-address-bar
-  if (navigator.userAgent.match(/iPhone|iPad|iPod/)) {
+  if (navigator.userAgent.match(/iPhone|iPod/)) {
     setTimeout(function() {
       window.scrollTo(0, 1);
     }, 0);
