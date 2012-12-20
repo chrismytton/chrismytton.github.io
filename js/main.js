08d8b54978c2b98b9611208dad0eb234f1709f93
Chris Mytton <self@hecticjeff.net>
2012-12-20 22:09:36 +0000

    Add fastclick polyfil

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-// Hide the address bar on iOS - http://davidwalsh.name/hide-address-bar
 window.addEventListener("load", function() {
-  setTimeout(function() {
-    window.scrollTo(0, 1);
-  }, 0);
-});
+  new FastClick(document.body);
+
+  // Hide the address bar on iOS - http://davidwalsh.name/hide-address-bar
+  if (navigator.userAgent.match(/iPhone|iPad|iPod/)) {
+    setTimeout(function() {
+      window.scrollTo(0, 1);
+    }, 0);
+  }
+}, false);
