b4b0c085dce856134c20e44c28fd1c283d383c71
Chris Mytton <chrismytton@gmail.com>
2014-12-25 10:52:55 +0000

    Reorganise and lint fingerprinting plugin

--- _plugins/fingerprint_assets.rb
+++ _plugins/fingerprint_assets.rb
@@ -1,18 +1,26 @@
 require 'digest'
 
+# Asset fingerprinting plugin
 module Jekyll
+  # If a page has `fingerprint: true` in the frontmatter then generate
+  # a version with an md5 in the filename.
   class AssetGenerator < Generator
     def generate(site)
       site.pages.select { |p| p.data['fingerprint'] }.each do |page|
-        md5 = Digest::MD5.hexdigest(page.transform)
-        fingerprinted_page = Page.new(site, site.source, page.dir, page.name)
-        fingerprinted_page.basename = "#{page.basename}-#{md5}"
-        fingerprinted_page.data = { 'original_url' => page.url }
-        site.pages.push(fingerprinted_page)
+        site.pages.push(fingerprint(page))
+      end
+    end
+
+    def fingerprint(page)
+      md5 = Digest::MD5.hexdigest(page.transform)
+      Page.new(page.site, page.site.source, page.dir, page.name).tap do |p|
+        p.basename = "#{page.basename}-#{md5}"
+        p.data['original_url'] = page.url
       end
     end
   end
 
+  # Template tag for producing the md5 url to an asset.
   class AssetUrlTag < Liquid::Tag
     def initialize(_tag_name, url, _tokens)
       @url = url.strip
@@ -24,6 +32,6 @@ module Jekyll
       page.url
     end
   end
-end
 
-Liquid::Template.register_tag('asset_url', Jekyll::AssetUrlTag)
+  Liquid::Template.register_tag('asset_url', Jekyll::AssetUrlTag)
+end
